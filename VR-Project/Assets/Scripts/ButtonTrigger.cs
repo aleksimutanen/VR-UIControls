3ca963560c5976870ec0a86a567692a65ecfbba2
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-06-14 10:19:42 +0300

    garbage

---
+++ VR-Project/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    public Transform spawnLocation;
+    public GameObject spawnObject;
+
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Cube") {
+            var b = Instantiate(spawnObject);
+            b.transform.position = spawnLocation.position;
+        }
+    }
+}
---
+++ VR-Project/Assets/Scripts/HapticPulse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticPulse : MonoBehaviour {
+
+    public bool hapticFlag = false;
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if (hapticFlag) {
+            device.TriggerHapticPulse(500);
+        }
+	}
+    void OnTriggerEnter() {
+        hapticFlag = true;
+    }
+    void OnTriggerExit() {
+        hapticFlag = false;
+    }
+}
+

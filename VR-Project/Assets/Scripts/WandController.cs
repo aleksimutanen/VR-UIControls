91603fe7658266ec7f22596dc11e660163205b62
sara <sokeritahra@gmail.com>
2018-06-14 13:44:03 +0300

    varasäätäjä paikalla

---
+++ VR-Project/Assets/Scripts/WandController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour {
+
+    //private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) )} }
+    private SteamVR_TrackedObject trackedObj;
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

5186458c6e57b768a59f3825d407009f24034a2c
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-06-14 15:02:59 +0300

    absolutely shit button panel

--- VR-Project/Assets/Scripts/ButtonTrigger.cs
+++ VR-Project/Assets/Scripts/ButtonTrigger.cs
@@ -6,15 +6,45 @@ public class ButtonTrigger : MonoBehaviour {
 
     public Transform spawnLocation;
     public GameObject spawnObject;
+    public GameObject[] buttons;
+    Renderer rend;
+    List<int> appliedNumbers = new List<int>();
+    public Light light;
 
 	void Start () {
-		
+        rend = GetComponent<Renderer>();
 	}
 
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.name == "Cube") {
-            var b = Instantiate(spawnObject);
-            b.transform.position = spawnLocation.position;
+        //if (other.gameObject.tag == "Button") {
+        //    var b = Instantiate(spawnObject);
+        //    b.transform.position = spawnLocation.position;
+        //}
+        for (int i = 0; i < buttons.Length; i++) {
+            if (other.gameObject == buttons[i]) {
+                buttons[i].GetComponent<Renderer>().material.color = Color.blue;
+                print("button " + buttons[i] + " hit");
+                appliedNumbers.Add(i + 1);
+                print("number " + (i + 1) + "added");
+            }
+        }
+        if (appliedNumbers.Contains(1)) {
+            if (appliedNumbers.Contains(3)) {
+                if (appliedNumbers.Contains(4)) {
+                    if (appliedNumbers.Contains(7)) {
+                        light.intensity = 25f;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        for (int i = 0; i < buttons.Length; i++) {
+            if (other.gameObject == buttons[i]) {
+                buttons[i].GetComponent<Renderer>().material.color = Color.red;
+            }
         }
     }
 }
+

27e2164d09ea8b069841d5f80441cea7686d300b
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-06-15 15:20:41 +0300

    Meshwalls and other sort of garbage

---
+++ VR-Project/Assets/Scripts/MeshSpawnPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSpawnPanel : MonoBehaviour {
+
+    public Transform spawnLocation;
+    public GameObject[] spawnObject;
+
+    void Start() {
+
+
+    }
+
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "MeshSpawnButton") {
+            var b = Instantiate(spawnObject[Random.Range(0, 9)]);
+            b.transform.position = spawnLocation.position + new Vector3(Random.Range(0f, 0.5f), 0, Random.Range(0f, 0.5f));
+            other.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "MeshSpawnButton") {
+            other.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
---
+++ VR-Project/Assets/Scripts/MeshTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTrigger : MonoBehaviour {
+
+    public string target;
+    public Light spotLight;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == target) {
+            print("xd");
+            spotLight.intensity = 5f;
+            spotLight.spotAngle = spotLight.spotAngle * 2;
+        }
+    }
+    //void OnTriggerEnter(Collider other) {
+    //    if (other.gameObject == targetMesh) {
+    //        print("xd");
+    //        spotLight.intensity = 10f;
+    //}
+}
+

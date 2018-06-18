728c096fae73bf6d169b7eb0c4ee2d9df3554f95
sara <sokeritahra@gmail.com>
2018-06-18 13:13:47 +0300

    ritsa alkaa

---
+++ VR-Project/Assets/Scripts/BuildRizza.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildRizza : MonoBehaviour {
+
+    bool pt2Done;
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        print("collision detected " + collision.gameObject);
+        if (collision.gameObject.name == "Rizza pt 2") {
+            print("rizzas touching " + collision.gameObject);
+            collision.gameObject.transform.parent = gameObject.transform;
+            collision.gameObject.transform.localScale = new Vector3()
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.MovePosition(transform.position);
+            
+            pt2Done = true;
+        }
+        if (collision.gameObject.name == "Rizza pt 3" && pt2Done) {
+            print("kolmas osa");
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+
+    }
+}

c664bf90cc8840bce0cfe7fb8301eea5e581e4e4
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-06-18 11:15:55 +0300

    Lever-script modified.

---
+++ VR-Project/Assets/LeverScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour {
+
+    public Transform hand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pointInPivotCoords = transform.InverseTransformPoint(hand.position);
+        var pointOnPlane = pointInPivotCoords; pointOnPlane.z = 0;
+        var pointInWorld = transform.TransformPoint(pointOnPlane);
+        transform.rotation = Quaternion.LookRotation(transform.forward, pointInWorld - transform.position);
+	}
+}

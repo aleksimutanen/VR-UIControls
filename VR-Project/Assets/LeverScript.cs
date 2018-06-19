0cc01b8ce60dbe03f08c0ba3411c47d2bb5ddaf4
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-06-19 13:06:09 +0300

    Button wall and Lever modified.

--- VR-Project/Assets/LeverScript.cs
+++ VR-Project/Assets/LeverScript.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LeverScript : MonoBehaviour {
 
     public Transform hand;
+    public float angle;
+    public Transform neutral;
+    public float maxAngle;
+    public UnityEvent switchOn;
+    public UnityEvent switchOff;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -16,6 +22,29 @@ public class LeverScript : MonoBehaviour {
         var pointInPivotCoords = transform.InverseTransformPoint(hand.position);
         var pointOnPlane = pointInPivotCoords; pointOnPlane.z = 0;
         var pointInWorld = transform.TransformPoint(pointOnPlane);
-        transform.rotation = Quaternion.LookRotation(transform.forward, pointInWorld - transform.position);
-	}
+        var newRot = Quaternion.LookRotation(transform.forward, pointInWorld - transform.position);
+        var newUp = newRot * Vector3.up;
+        var newAngle = Vector3.Angle(newUp, neutral.up);
+        if (newAngle > maxAngle)
+            return;
+        transform.rotation = newRot;
+        angle = Vector3.Angle(transform.up, neutral.up);
+        var dir = neutral.InverseTransformDirection(transform.up);
+        if (dir.x <= 0) {
+            angle *= -1;
+        }
+
+        if (angle >= 30) {
+            print("jes");
+            switchOn.Invoke();
+        } else if (angle <= -30) {
+            print("jos");
+            switchOff.Invoke(); }
+        // did we just move to activation zone?
+        // if ...
+        // switchOn.Invoke();
+        // switchOff.Invoke();
+
+
+    }
 }

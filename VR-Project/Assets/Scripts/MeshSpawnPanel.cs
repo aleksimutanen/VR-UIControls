d191e0b3125028cf2d389908876f706de6e7fdc6
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-06-19 11:19:22 +0300

    fixed mesh spawning, modded walls to stop mesh flickering

--- VR-Project/Assets/Scripts/MeshSpawnPanel.cs
+++ VR-Project/Assets/Scripts/MeshSpawnPanel.cs
@@ -6,6 +6,9 @@ public class MeshSpawnPanel : MonoBehaviour {
 
     public Transform spawnLocation;
     public GameObject[] spawnObject;
+    public int meshSpawn;
+    public int maxWrongThreshold;
+    int index = 0;
 
     void Start() {
 
@@ -17,10 +20,17 @@ public class MeshSpawnPanel : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.name == "MeshSpawnButton") {
+        if (meshSpawn < maxWrongThreshold && other.gameObject.name == "MeshSpawnButton") {
+            print("spawn random");
             var b = Instantiate(spawnObject[Random.Range(0, 9)]);
             b.transform.position = spawnLocation.position + new Vector3(Random.Range(0f, 0.5f), 0, Random.Range(0f, 0.5f));
+            meshSpawn++;
             other.GetComponent<Renderer>().material.color = Color.red;
+        } else if (meshSpawn >= maxWrongThreshold && other.gameObject.name == "MeshSpawnButton") {
+            print("spawn correct");
+            var c = Instantiate(spawnObject[0 + index]);
+            c.transform.position = spawnLocation.position + new Vector3(Random.Range(0f, 0.5f), 0, Random.Range(0f, 0.5f));
+            index++;
         }
     }
 
